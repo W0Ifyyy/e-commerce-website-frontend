7d164d0835958ba7a6ad8e9e5192026ec402ca0b
W0Ifyyy <bartosz.wilk.123@hotmail.com>
2026-01-23 18:10:04 +0100

    refactor: remove footer links for privacy policy, terms of service, and contact us

--- components/rootLayout/Footer.tsx
+++ components/rootLayout/Footer.tsx
@@ -4,17 +4,6 @@ export default function Footer() {
   return (
     <footer className="bg-gray-200 p-4 w-full shadow flex flex-col sm:flex-row justify-center gap-5 sm:gap-20 items-center text-sm sm:text-base">
       <p className="m-0 text-gray-500">© W0Ifyy 2025</p>
-      <div className="flex space-x-4">
-        <a href="/privacy-policy" className="text-gray-500 hover:underline">
-          Privacy Policy
-        </a>
-        <a href="/terms-of-service" className="text-gray-500 hover:underline">
-          Terms of Service
-        </a>
-        <a href="/contact-us" className="text-gray-500 hover:underline">
-          Contact Us
-        </a>
-      </div>
     </footer>
   );
 }
